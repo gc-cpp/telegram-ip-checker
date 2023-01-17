8441dec5fd5e85799f3f8bb0755c6a3c6423be8b
Sasha Leonov <leonov22.a.v@gmail.com>
2023-01-17 20:08:11 +0700

    [main] Hello github.

---
+++ TelegramApiChecker/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace TelegramApiChecker
+{
+    internal static class Utils
+    {
+        public static string GetTelegramApiToken()
+        {
+            var telegramApi = Environment.GetEnvironmentVariable("TELEGRAM_API");
+            ArgumentNullException.ThrowIfNull(telegramApi, "TELEGRAM_API");
+            return telegramApi;
+        }
+    }
+}
